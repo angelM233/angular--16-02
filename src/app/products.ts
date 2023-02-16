b5470e628a6891fab2560dc6223638821fc0f6a5
angelM233 <124307618+angelM233@users.noreply.github.com>
2023-02-16 15:14:47 -0600

    6 files modified, 4 added

--- src/app/products.ts
+++ src/app/products.ts
@@ -3,6 +3,7 @@ export interface Product {
   name: string;
   price: number;
   description: string;
+  stock: number;
 }
 
 export const products = [
@@ -10,19 +11,29 @@ export const products = [
     id: 1,
     name: 'Phone XL',
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: 'A large phone with one of the best screens',
+    stock: 0,
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    stock: 0,
   },
   {
     id: 3,
-    name: 'Phone Standard',
-    price: 299,
-    description: ''
+    name: 'Xiaomi',
+    price: 1000,
+    description: 'Redmi note 8',
+    stock: 0,
+  },
+  {
+    id: 3,
+    name: 'Alcatel',
+    price: 1000,
+    description: '',
+    stock: 0,
   }
 ];
 
